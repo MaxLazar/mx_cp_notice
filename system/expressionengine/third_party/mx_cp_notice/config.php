12b2850ffa5195171851b89c485fb385ca563147
Max Lazar <max@eec.ms>
2013-11-19 02:35:39 +0400

    no message

--- system/expressionengine/third_party/mx_cp_notice/config.php
+++ system/expressionengine/third_party/mx_cp_notice/config.php
@@ -2,7 +2,7 @@
 if (! defined('MX_CP_NOTICE_KEY'))
 {
 	define('MX_CP_NOTICE_NAME', 'MX CP Notice');
-	define('MX_CP_NOTICE_VER',  '0.6');
+	define('MX_CP_NOTICE_VER',  '0.9');
 	define('MX_CP_NOTICE_KEY', 'Mx_cp_notice');
 	define('MX_CP_NOTICE_AUTHOR',  'Max Lazar');
 	define('MX_CP_NOTICE_DOCS',  '');
